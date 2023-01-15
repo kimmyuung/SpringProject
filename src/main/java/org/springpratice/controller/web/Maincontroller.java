a693bab3ab99066324c9dd6cf7bf52c60080bcc5
kimmyungho <kimmyungho1003@naver.com>
2023-01-15 18:50:38 +0900

    Mustache를 통해 게시판 API 구현하기

--- src/main/java/org/springpratice/controller/web/Maincontroller.java
+++ src/main/java/org/springpratice/controller/web/Maincontroller.java
@@ -1,20 +1,23 @@
 package org.springpratice.controller.web;
 
+import lombok.RequiredArgsConstructor;
 import org.aspectj.weaver.ast.Test;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springpratice.controller.web.dto.PostsResponseDto;
 import org.springpratice.controller.web.dto.TestResponseDto;
+import org.springpratice.service.posts.PostsService;
 
 //@RestController
 // 컨트롤러를 JSON을 반환하는 컨트롤러로 만들어줌
 // @ResponseBody를 한번에 사용해 줄 수 있게 된다고 보면 됨
 
 @Controller
+@RequiredArgsConstructor
 public class Maincontroller {
+
+    private final PostsService postService;
 
     // 테스트용
     @GetMapping("/test") // HTTP Method인 Get의 요청을 받을 수 있는 API를 만들어줌
@@ -38,6 +41,25 @@ public class Maincontroller {
         return "/index";
     }
 
+    @GetMapping("/index2") // HTTP Method인 Get의 요청을 받을 수 있는 API를 만들어줌
+    public String index2(Model model) {
+        model.addAttribute("posts", postService.findAllDesc());
+        return "/index2";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+
+        PostsResponseDto dto = postService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
+    }
 
 
     @GetMapping("/contextmodule")
--- src/main/java/org/springpratice/service/posts/PostsService.java
+++ src/main/java/org/springpratice/service/posts/PostsService.java
@@ -3,11 +3,15 @@ package org.springpratice.service.posts;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springpratice.controller.web.dto.PostsListResponseDto;
 import org.springpratice.controller.web.dto.PostsResponseDto;
 import org.springpratice.controller.web.dto.PostsSaveRequestDto;
 import org.springpratice.controller.web.dto.PostsUpdateRequestDto;
 import org.springpratice.domain.posts.Posts;
 import org.springpratice.domain.posts.PostsRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,4 +39,21 @@ public class PostsService {
 
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다 id=" + id));
+
+        postRepository.delete(posts);
+    }
+
+
 }
